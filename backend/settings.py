02189a77999b7e68687767795c6ba92edbee4164
Senoaji Pamungkas <76572178+senoo12@users.noreply.github.com>
2025-06-26 22:27:51 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$m-o%h@d3^3&xz89i!yhr6g#%-s^hfde81g@p(kgkzl9wkbs38'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,8 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'serverreseecle@gmail.com'
-EMAIL_HOST_PASSWORD = 'spmb khys vusz yjok'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SIMPLE_JWT = {
